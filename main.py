f6cc316e256acd156c99c0a6c2069d2ecc259e29
bobbyhadz <opmftw@gmail.com>
2024-04-10 09:07:56 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Convert letters to numbers in Python
+
+number = ord('a')
+print(number)  # 👉️ 97
+
+one_based = ord('a') - 96
+print(one_based)  # 👉️ 1
+
+# ------------------------------------
+
+number = ord('A')
+print(number)  # 👉️ 65
+
+one_based = ord('A') - 64
+print(one_based)  # 👉️ 1
